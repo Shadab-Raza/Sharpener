18a6a44c817e93387f2e3df7ea886287a72caa26
shadab1928raza <shadab.razamd@gmail.com>
2023-02-05 23:15:43 +0530

    Question-3 done

--- DOM_1.js
+++ DOM_1.js
@@ -2,39 +2,60 @@
 
     // Selecting the Document
 // console.dir(document);
-console.log(document.domain)
-console.log(document.URL)
-console.log(document.title);
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title);
 
-// Changing the title
+    // Changing the title
 //document.title = 123;
-//document.title = "My Site"
+//document.title = "My Site";
 
-console.log (document.doctype);
-console.log (document.head);
-console.log (document.body);
-console.log (document.all);
-console.log (document.all[10]);
+// console.log (document.doctype);
+// console.log (document.head);
+// console.log (document.body);
+// console.log (document.all);
+// console.log (document.all[10]);
 
 //document.all[10].textContent = 'Hello';
 
-console.log(document.forms);
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images)
+// console.log(document.forms);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
 
 
-// Using Selector for Selecting the Elements;
+// Using Different Selector for Selecting the Elements;
 
-    // * GETELEMENTBYID
+// * GETELEMENTBYID
+
     //M-1 (Without Using Variable)
-    console.log(document.getElementById('header-title'));
+    // console.log(document.getElementById('header-title'));
 
     //M-2 (By Using Variable)
     var headerTitle = document.getElementById('header-title');
-    console.log (headerTitle);
+    var header = document.getElementById('main-header');
+    // console.log (headerTitle);
 
-    headerTitle.textContent = 'Hello'
-    headerTitle.innerHTML = 'Welcome to Sharpener';
-    headerTitle.style.color = 'black';
-    headerTitle.style.fontStyle = 'italic';
+    // headerTitle.textContent = 'Hello';  ---> Method one to write from js
+    // headerTitle.innerHTML = 'Welcome to Sharpener';    ---> Method two to write from js
+
+    //headerTitle.style.color = 'black';
+    //headerTitle.style.fontStyle = 'italic';
+
+    //console.log(headerTitle.innerHTML);
+    // headerTitle.innerHTML = '<h3>Hello</h3>';   ---> Method three to write from js/
+
+    header.style.border = 'solid 3px black';
+    
+
+// * GETELEMENTBYCLASSNAME
+
+    // Q.No 3
+
+    var title = document.getElementsByClassName('title');
+    console.log(title);
+
+    console.log(title[0]);
+    //title[0].textContent = "Hello";
+    title[0].style.fontWeight = "bold";
+    title[0].style.color = "green";
