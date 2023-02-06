3139e18e6ddef5b7b3df12f7d20404edb34c4a79
shadab1928raza <shadab.razamd@gmail.com>
2023-02-06 14:02:14 +0530

    TASK-5 done

--- DOM_1.js
+++ DOM_1.js
@@ -1,6 +1,9 @@
+//--------------------------------TASK-3-----------------------------------------------------
+
 // Task 3 Question No - 2
 
     // Selecting the Document
+
 // console.dir(document);
 // console.log(document.domain)
 // console.log(document.URL)
@@ -22,6 +25,7 @@
 // console.log(document.forms[0]);
 // console.log(document.links);
 // console.log(document.images);
+
 
 
 // Using Different Selector for Selecting the Elements;
@@ -49,6 +53,7 @@
     // header.style.border = 'solid 3px black';
     
 
+
 // * GETELEMENTBYCLASSNAME
 //-------------------------------------------------
     // Q.No 3
@@ -62,29 +67,78 @@
     // title[0].style.color = "green";
 
 
+
+//----------------------------------TASK-4----------------------------------------------------------------------
+
      // TASK-4 Question-1
 
-    var items = document.getElementsByClassName('list-group-item');
-    console.log(items);
+    // var items = document.getElementsByClassName('list-group-item');
+    // console.log(items);
 
-    items[1].textContent = 'Second Item';
-    items[1].style.fontWeight = 'bold';
-    items[1].style.backgroundColor = 'yellow';
+    // items[1].textContent = 'Second Item';
+    // items[1].style.fontWeight = 'bold';
+    // items[1].style.backgroundColor = 'yellow';
 
-    for (var i = 0; i < items.length; i++) {
-        items[i].style.backgroundColor = '#f4f4f4';
-    }
+    // for (var i = 0; i < items.length; i++) {
+    //     items[i].style.backgroundColor = '#f4f4f4';
+    // }
 
 
     // TASK-4 Question-2
-    items[2].textContent = 'Third Item';
-    items[2].style.fontWeight = 'bold';
-    items[2].style.backgroundColor = 'green';
+    // items[2].textContent = 'Third Item';
+    // items[2].style.fontWeight = 'bold';
+    // items[2].style.backgroundColor = 'green';
 
 
 
     // TASK-4 Question-3
-    items[0].textContent = 'First Item';
-    items[0].style.fontWeight = 'bold';
-    items[3].textContent = 'Fourth Item';
-    items[3].style.fontWeight = 'bold';
+    // items[0].textContent = 'First Item';
+    // items[0].style.fontWeight = 'bold';
+    // items[3].textContent = 'Fourth Item';
+    // items[3].style.fontWeight = 'bold';
+
+
+
+//-----------------------------------------TASK-5------------------------------------------------------------------
+
+    // TASK-5 Playing with li with different className
+    // var groupItem = document.getElementsByClassName('group_item');
+    // console.log (groupItem); 
+    // console.log (groupItem[0]); 
+
+    // groupItem[0].textContent = "Item Five Manual";
+    // groupItem[0].style.listStyle = 'none';
+    // groupItem[0].style.fontWeight = 'bold';
+    // groupItem[0].style.backgroundColor = 'green';
+    // groupItem[0].style.color = 'white';
+    // groupItem[0].style.padding = '15px 22px';
+    // groupItem[0].style.border = '1px solid #cccc';
+    // groupItem[0].style.borderRadius = '4px';
+
+
+
+
+// * GETELEMENTBYTAGNAME
+//-------------------------------------------------
+
+    // Task-5
+    var li = document.getElementsByTagName('li');
+    console.log(li);
+    console.log(li[4]);
+
+    // li[1].textContent = 'Second Item';
+    // li[1].style.fontWeight = 'bold';
+    // li[1].style.backgroundColor = 'yellow';
+
+    // for (var i = 0; i < li.length; i++) {
+    //     li[i].style.backgroundColor = '#f4f4f4';
+    // }
+
+    li[4].style.listStyle = 'none';
+    li[4].style.fontWeight = 'bold';
+    li[4].style.backgroundColor = 'green';
+    li[4].style.color = 'white';
+    li[4].style.padding = '15px 22px';
+    li[4].style.border = '1px solid #cccc';
+    li[4].style.borderRadius = '4px';
+
