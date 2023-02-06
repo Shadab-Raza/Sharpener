f331d28d1aa4b63790525276539113dff784281b
shadab1928raza <shadab.razamd@gmail.com>
2023-02-06 14:52:35 +0530

    TASK-6 completed

--- DOM_1.js
+++ DOM_1.js
@@ -122,9 +122,9 @@
 //-------------------------------------------------
 
     // Task-5
-    var li = document.getElementsByTagName('li');
-    console.log(li);
-    console.log(li[4]);
+    // var li = document.getElementsByTagName('li');
+    // console.log(li);
+    // console.log(li[4]);
 
     // li[1].textContent = 'Second Item';
     // li[1].style.fontWeight = 'bold';
@@ -134,11 +134,76 @@
     //     li[i].style.backgroundColor = '#f4f4f4';
     // }
 
-    li[4].style.listStyle = 'none';
-    li[4].style.fontWeight = 'bold';
-    li[4].style.backgroundColor = 'green';
-    li[4].style.color = 'white';
-    li[4].style.padding = '15px 22px';
-    li[4].style.border = '1px solid #cccc';
-    li[4].style.borderRadius = '4px';
+    // li[4].style.listStyle = 'none';
+    // li[4].style.fontWeight = 'bold';
+    // li[4].style.backgroundColor = 'green';
+    // li[4].style.color = 'white';
+    // li[4].style.padding = '15px 22px';
+    // li[4].style.border = '1px solid #cccc';
+    // li[4].style.borderRadius = '4px';
 
+
+
+//-----------------------------------------TASK-6------------------------------------------------------------------
+
+// * QUERYSELECTOR
+//-------------------------------------------------
+
+    // var header = document.querySelector('#main-header');
+    // console.log (header);
+
+    // header.style.borderBottom = 'solid 4px #000000';
+
+    // var input = document.querySelector('input');
+    // input.value = "Hello World";
+
+    // var submit = document.querySelector('input[type="submit"]');
+    // submit.value = "SEND";
+
+    // var item = document.querySelector('.list-group-item');
+    // item.style.color = "red";
+
+    // var lastItem = document.querySelector('.list-group-item:last-child');
+    // lastItem.style.color = 'blue';
+
+    // var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+    // secondItem.style.color = 'orange';
+
+
+// TASK-6 Question-1 Solution
+
+    // var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+    // var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+
+    // secondItem.style.backgroundColor = 'green';
+    // thirdItem.style.display = 'none';
+
+
+// * QUERYSELECTORALL
+//-------------------------------------------------
+
+    // var titles = document.querySelectorAll('.title');
+    // console.log(titles);
+
+    // titles[0].textContent = 'Hello';
+
+    // var odd = document.querySelectorAll ('li:nth-child(odd');
+    // var even = document.querySelectorAll ('li:nth-child(even');
+
+    // for(var i = 0; i < odd.length; i++) {
+    //     odd[i].style.backgroundColor = '#f4f4f4';
+    //     even[i].style.backgroundColor = '#ccc';
+    // }
+
+
+// TASK-6 Question-2 Solution
+
+    var secondItem = document.querySelectorAll('li:nth-child(2)');
+    console.log (secondItem);
+
+    secondItem[0].style.color = 'green';
+
+    var odd = document.querySelectorAll ('li:nth-child(odd');
+    for(var i = 0; i < odd.length; i++) {
+            odd[i].style.backgroundColor = 'green';
+        }
